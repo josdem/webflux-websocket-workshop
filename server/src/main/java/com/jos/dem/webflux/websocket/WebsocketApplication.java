447205725cf8ac4521c7bf523286f8100fa30d8c
josdem <joseluis.delacruz@gmail.com>
2020-02-26 12:49:59 -0500

    #1 Cleaning up the code, incorporate JsonNode

--- server/src/main/java/com/jos/dem/webflux/websocket/WebsocketApplication.java
+++ server/src/main/java/com/jos/dem/webflux/websocket/WebsocketApplication.java
@@ -1,10 +1,8 @@
 package com.jos.dem.webflux.websocket;
 
-import com.jos.dem.webflux.websocket.handler.ReactiveWebSocketHandler;
-import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
-
 import java.util.HashMap;
 import java.util.Map;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,16 +11,13 @@ import org.springframework.core.Ordered;
 import org.springframework.web.reactive.HandlerMapping;
 import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
 import org.springframework.web.reactive.socket.WebSocketHandler;
-import org.springframework.web.reactive.socket.server.WebSocketService;
-import org.springframework.web.reactive.socket.server.support.HandshakeWebSocketService;
 import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
-import org.springframework.web.reactive.socket.server.upgrade.ReactorNettyRequestUpgradeStrategy;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class WebsocketApplication {
 
-  @Autowired
-  private WebSocketHandler webSocketHandler;
+  private final WebSocketHandler webSocketHandler;
 
   public static void main(String[] args) {
     SpringApplication.run(WebsocketApplication.class, args);
@@ -43,5 +38,4 @@ public class WebsocketApplication {
   public WebSocketHandlerAdapter handlerAdapter() {
     return new WebSocketHandlerAdapter();
   }
-
 }
