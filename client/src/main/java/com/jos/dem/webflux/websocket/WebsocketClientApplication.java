42757b7d1940f7d831e6ac8f682faf76be394cae
josdem <joseluis.delacruz@gmail.com>
2020-02-27 11:48:36 -0500

    #2 Adding subscribe method

--- client/src/main/java/com/jos/dem/webflux/websocket/WebsocketClientApplication.java
+++ client/src/main/java/com/jos/dem/webflux/websocket/WebsocketClientApplication.java
@@ -1,16 +1,20 @@
 package com.jos.dem.webflux.websocket;
 
+import java.net.URI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.socket.WebSocketHandler;
 import org.springframework.web.reactive.socket.client.WebSocketClient;
-
-import java.net.URI;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class WebsocketClientApplication {
+
+  private Logger log = LoggerFactory.getLogger(this.getClass());
 
   public static void main(String[] args) {
     SpringApplication.run(WebsocketClientApplication.class, args);
@@ -19,7 +23,12 @@ public class WebsocketClientApplication {
   @Bean
   CommandLineRunner run(WebSocketClient client, WebSocketHandler webSocketHandler) {
     return args -> {
-      client.execute(URI.create("ws://localhost:8080/channel"), webSocketHandler).block();
-    };
+      Mono<Void> subscriber = client.execute(URI.create("ws://localhost:8080/channel"), webSocketHandler);
+
+      subscriber.subscribe(
+          content -> log.info("content"),
+          error -> log.error("Error at receiving events: {}", error),
+          () -> log.info("complete"));
+      };
   }
 }
