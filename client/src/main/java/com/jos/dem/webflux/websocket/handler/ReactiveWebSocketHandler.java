9cf86680971899ef80080b09d21854b7a449d90b
josdem <joseluis.delacruz@gmail.com>
2020-02-27 10:57:14 -0500

    #2 Adding event as Json payload

--- client/src/main/java/com/jos/dem/webflux/websocket/handler/ReactiveWebSocketHandler.java
+++ client/src/main/java/com/jos/dem/webflux/websocket/handler/ReactiveWebSocketHandler.java
@@ -1,5 +1,9 @@
 package com.jos.dem.webflux.websocket.handler;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jos.dem.webflux.websocket.model.Event;
+import java.time.Instant;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.socket.WebSocketHandler;
 import org.springframework.web.reactive.socket.WebSocketMessage;
@@ -9,10 +13,17 @@ import reactor.core.publisher.Mono;
 @Component
 public class ReactiveWebSocketHandler implements WebSocketHandler {
 
+  private final ObjectMapper mapper = new ObjectMapper();
+
   @Override
   public Mono<Void> handle(WebSocketSession session) {
     return session
-        .send(Mono.just(session.textMessage("event")))
+        .send(Mono.just(session.textMessage(getEvent())))
         .and(session.receive().map(WebSocketMessage::getPayloadAsText).log());
+  }
+
+  private String getEvent(){
+    JsonNode node = mapper.valueToTree(new Event("start", Instant.now()));
+    return node.toString();
   }
 }
