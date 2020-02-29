2c0b3160c4481f84f9e684f432201b7edd56432b
josdem <joseluis.delacruz@gmail.com>
2020-02-29 12:52:28 -0500

    #4 Adding cache in application state

---
+++ server/src/main/java/com/jos/dem/webflux/websocket/util/ApplicationState.java
@@ -0,0 +1,9 @@
+package com.jos.dem.webflux.websocket.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ApplicationState {
+
+  public static final List<String> cache = new ArrayList<>();
+}
