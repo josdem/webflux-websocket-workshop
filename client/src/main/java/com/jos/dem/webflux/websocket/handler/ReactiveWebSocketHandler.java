e84f6bb1577e284bcb26a61791e72940a47b215c
josdem <joseluis.delacruz@gmail.com>
2020-03-12 18:08:31 -0400

    #5 Adding receiver processor

--- client/src/main/java/com/jos/dem/webflux/websocket/handler/ReactiveWebSocketHandler.java
+++ client/src/main/java/com/jos/dem/webflux/websocket/handler/ReactiveWebSocketHandler.java
@@ -14,6 +14,8 @@ import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.WebSocketSession;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.MonoProcessor;
+import reactor.core.publisher.ReplayProcessor;
 
 @Component
 @RequiredArgsConstructor
@@ -24,6 +26,10 @@ public class ReactiveWebSocketHandler implements WebSocketHandler {
   private Flux<String> intervalSilenceFlux;
   private final MessageGenerator messageGenerator;
   private final ObjectMapper mapper = new ObjectMapper();
+  private MonoProcessor<WebSocketSession> connectedProcessor;
+  private WebSocketSession session;
+  private ReplayProcessor<String> receiveProcessor;
+  private boolean webSocketConnected = false;
 
   @PostConstruct
   private void setup(){
@@ -32,13 +38,34 @@ public class ReactiveWebSocketHandler implements WebSocketHandler {
     intervalSilenceFlux =
         Flux.interval(Duration.ofSeconds(1)).map(it -> getStream());
     emiterFlux = intervalNameFlux;
+    connectedProcessor = MonoProcessor.create();
+    receiveProcessor = ReplayProcessor.create(50);
   }
 
   @Override
   public Mono<Void> handle(WebSocketSession session) {
-    return session
-        .send(emiterFlux.map(session::textMessage))
-        .and(session.receive().map(message -> message.getPayloadAsText()).log());
+    this.session = session;
+
+    Mono<Object> connected =
+        Mono
+            .fromRunnable(() ->
+            {
+              webSocketConnected = true;
+              connectedProcessor.onNext(session);
+            });
+
+    Mono<Void> send =
+        session
+          .send(Mono.just(session.textMessage(getSilence())));
+
+    Flux<String> receive =
+        session
+            .receive()
+            .map(message -> message.getPayloadAsText())
+            .doOnNext(textMessage -> receiveProcessor.onNext(textMessage))
+            .doOnComplete(() -> receiveProcessor.onComplete());
+
+    return connected.then(send).thenMany(receive).then();
   }
 
   private String getSilence(){
