d25f0fa2d6b02f31a323a9e7115d504a60722b56
josdem <joseluis.delacruz@gmail.com>
2020-03-16 12:55:04 -0400

    #5 Deleting unused code

--- client/src/main/java/com/jos/dem/webflux/websocket/handler/ReactiveWebSocketHandler.java
+++ client/src/main/java/com/jos/dem/webflux/websocket/handler/ReactiveWebSocketHandler.java
@@ -8,6 +8,8 @@ import java.time.Duration;
 import java.time.Instant;
 import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.socket.WebSocketHandler;
 import org.springframework.web.reactive.socket.WebSocketMessage;
@@ -21,39 +23,23 @@ import reactor.core.publisher.ReplayProcessor;
 @RequiredArgsConstructor
 public class ReactiveWebSocketHandler implements WebSocketHandler {
 
-  private Flux<String> emiterFlux;
   private Flux<String> intervalNameFlux;
   private Flux<String> intervalSilenceFlux;
   private final MessageGenerator messageGenerator;
   private final ObjectMapper mapper = new ObjectMapper();
-  private MonoProcessor<WebSocketSession> connectedProcessor;
-  private WebSocketSession session;
-  private ReplayProcessor<String> receiveProcessor;
-  private boolean webSocketConnected = false;
+
+  private Logger log = LoggerFactory.getLogger(this.getClass());
 
   @PostConstruct
   private void setup(){
-    intervalNameFlux =
-        Flux.interval(Duration.ofSeconds(1)).map(it -> getSilence());
     intervalSilenceFlux =
+        Flux.interval(Duration.ofSeconds(1)).map(it -> getSilence());
+    intervalNameFlux =
         Flux.interval(Duration.ofSeconds(1)).map(it -> getStream());
-    emiterFlux = intervalNameFlux;
-    connectedProcessor = MonoProcessor.create();
-    receiveProcessor = ReplayProcessor.create(50);
   }
 
   @Override
   public Mono<Void> handle(WebSocketSession session) {
-    this.session = session;
-
-    Mono<Object> connected =
-        Mono
-            .fromRunnable(() ->
-            {
-              webSocketConnected = true;
-              connectedProcessor.onNext(session);
-            });
-
     Mono<Void> send =
         session
           .send(Mono.just(session.textMessage(getSilence())));
@@ -62,10 +48,10 @@ public class ReactiveWebSocketHandler implements WebSocketHandler {
         session
             .receive()
             .map(message -> message.getPayloadAsText())
-            .doOnNext(textMessage -> receiveProcessor.onNext(textMessage))
-            .doOnComplete(() -> receiveProcessor.onComplete());
+            .doOnNext(textMessage -> log.info("message: {}", textMessage))
+            .doOnComplete(() -> log.info("complete"));
 
-    return connected.then(send).thenMany(receive).then();
+    return send.thenMany(receive).then();
   }
 
   private String getSilence(){
