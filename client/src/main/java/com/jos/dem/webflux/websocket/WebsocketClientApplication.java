7d42a6ddb8bde45120004546c2ff6f528f7d0af3
josdem <joseluis.delacruz@gmail.com>
2020-03-16 15:38:29 -0400

    #5 Creating session handler

--- client/src/main/java/com/jos/dem/webflux/websocket/WebsocketClientApplication.java
+++ client/src/main/java/com/jos/dem/webflux/websocket/WebsocketClientApplication.java
@@ -1,15 +1,9 @@
 package com.jos.dem.webflux.websocket;
 
-import java.net.URI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.reactive.socket.WebSocketHandler;
-import org.springframework.web.reactive.socket.client.WebSocketClient;
-import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class WebsocketClientApplication {
@@ -18,17 +12,5 @@ public class WebsocketClientApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(WebsocketClientApplication.class, args);
-  }
-
-  @Bean
-  CommandLineRunner run(WebSocketClient client, WebSocketHandler webSocketHandler) {
-    return args -> {
-      Mono<Void> subscriber = client.execute(URI.create("ws://localhost:8080/channel"), webSocketHandler);
-
-      subscriber.subscribe(
-          content -> log.info("content"),
-          error -> log.error("Error at receiving events: {}", error),
-          () -> log.info("complete"));
-      };
   }
 }
--- client/src/main/java/com/jos/dem/webflux/websocket/handler/ReactiveWebSocketHandler.java
+++ client/src/main/java/com/jos/dem/webflux/websocket/handler/ReactiveWebSocketHandler.java
@@ -3,69 +3,48 @@ package com.jos.dem.webflux.websocket.handler;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jos.dem.webflux.websocket.model.Event;
-import com.jos.dem.webflux.websocket.util.MessageGenerator;
-import java.time.Duration;
-import java.time.Instant;
-import javax.annotation.PostConstruct;
-import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.socket.WebSocketHandler;
-import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.WebSocketSession;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.core.publisher.MonoProcessor;
 import reactor.core.publisher.ReplayProcessor;
 
-@Component
-@RequiredArgsConstructor
+import javax.annotation.PostConstruct;
+import java.time.Instant;
+
 public class ReactiveWebSocketHandler implements WebSocketHandler {
 
-  private Flux<String> intervalNameFlux;
-  private Flux<String> intervalSilenceFlux;
-  private final MessageGenerator messageGenerator;
+  private WebSocketSessionHandler sessionHandler;
+  private ReplayProcessor<WebSocketSessionHandler> connectedProcessor;
   private final ObjectMapper mapper = new ObjectMapper();
 
   private Logger log = LoggerFactory.getLogger(this.getClass());
 
   @PostConstruct
-  private void setup() {
-    intervalSilenceFlux = Flux.interval(Duration.ofSeconds(1)).map(it -> getSilence());
-    intervalNameFlux = Flux.interval(Duration.ofSeconds(1)).map(it -> getStream());
+  private void setup(){
+    sessionHandler = new WebSocketSessionHandler();
+    connectedProcessor = ReplayProcessor.create();
   }
 
   @Override
   public Mono<Void> handle(WebSocketSession session) {
-    Flux<String> receive =
-        session
-            .receive()
-            .map(message -> message.getPayloadAsText())
-            .doOnNext(
-                textMessage -> {
-                  log.info("message: {}", textMessage);
-                  if (textMessage.contains("Hola")) {
-                    log.info("Hola");
-                    Mono.fromRunnable(() -> send(session));
-                  }
-                })
-            .doOnComplete(() -> log.info("complete"));
+    sessionHandler
+            .connected()
+            .doOnNext(value -> connectedProcessor.onNext(sessionHandler))
+            .subscribe();
 
-    return send(session).thenMany(receive).then();
+    return sessionHandler.handle(session);
   }
 
-  private Mono<Void> send(WebSocketSession session) {
-    return session.send(Mono.just(session.textMessage(getSilence())));
-  }
-
-  private String getSilence() {
+  private String getMessage() {
     JsonNode node = mapper.valueToTree(new Event("silence", Instant.now()));
     return node.toString();
   }
 
-  private String getStream() {
-    JsonNode node = mapper.valueToTree(new Event(messageGenerator.generate(), Instant.now()));
-    return node.toString();
+  public Flux<WebSocketSessionHandler> connected() {
+    return connectedProcessor;
   }
+
 }
