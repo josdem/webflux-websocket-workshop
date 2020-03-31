421bf61d095ecd3429993111f8d52f9fc758b005
josdem <joseluis.delacruz@gmail.com>
2020-03-30 20:00:44 -0400

    #6 Implementing simple echo server

--- server/src/main/java/com/jos/dem/webflux/websocket/handler/ReactiveWebSocketHandler.java
+++ server/src/main/java/com/jos/dem/webflux/websocket/handler/ReactiveWebSocketHandler.java
@@ -4,16 +4,16 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jos.dem.webflux.websocket.model.Event;
 import com.jos.dem.webflux.websocket.util.MessageGenerator;
-import java.time.Duration;
-import java.time.Instant;
-import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.socket.WebSocketHandler;
-import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.WebSocketSession;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import javax.annotation.PostConstruct;
+import java.time.Duration;
+import java.time.Instant;
 
 @Component
 @RequiredArgsConstructor
@@ -24,19 +24,21 @@ public class ReactiveWebSocketHandler implements WebSocketHandler {
   private final MessageGenerator messageGenerator;
 
   @PostConstruct
-  private void setup(){
-    intervalFlux =
-        Flux.interval(Duration.ofSeconds(2)).map(it -> getEvent());
+  private void setup() {
+    intervalFlux = Flux.interval(Duration.ofSeconds(2)).map(it -> getEvent());
   }
 
   @Override
   public Mono<Void> handle(WebSocketSession session) {
-    return session
-        .send(intervalFlux.map(session::textMessage))
-        .and(session.receive().map(WebSocketMessage::getPayloadAsText).log());
+    return session.send(
+        session
+            .receive()
+            .map(webSocketMessage -> webSocketMessage.getPayloadAsText())
+            .log()
+            .map(message -> session.textMessage(message)));
   }
 
-  private String getEvent(){
+  private String getEvent() {
     JsonNode node = mapper.valueToTree(new Event(messageGenerator.generate(), Instant.now()));
     return node.toString();
   }
