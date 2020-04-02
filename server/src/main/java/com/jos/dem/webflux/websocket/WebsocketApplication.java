248327b0a0768e3d77578f5c8195e8bf38513a8b
josdem <joseluis.delacruz@gmail.com>
2020-04-01 20:41:53 -0400

    #8 Removing beans from main application class

--- server/src/main/java/com/jos/dem/webflux/websocket/WebsocketApplication.java
+++ server/src/main/java/com/jos/dem/webflux/websocket/WebsocketApplication.java
@@ -1,41 +1,12 @@
 package com.jos.dem.webflux.websocket;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.Ordered;
-import org.springframework.web.reactive.HandlerMapping;
-import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
-import org.springframework.web.reactive.socket.WebSocketHandler;
-import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootApplication
-@RequiredArgsConstructor
 public class WebsocketApplication {
-
-  private final WebSocketHandler webSocketHandler;
 
   public static void main(String[] args) {
     SpringApplication.run(WebsocketApplication.class, args);
-  }
-
-  @Bean
-  public HandlerMapping handlerMapping() {
-    Map<String, WebSocketHandler> map = new HashMap<>();
-    map.put("/channel", webSocketHandler);
-
-    SimpleUrlHandlerMapping mapping = new SimpleUrlHandlerMapping();
-    mapping.setOrder(Ordered.HIGHEST_PRECEDENCE);
-    mapping.setUrlMap(map);
-    return mapping;
-  }
-
-  @Bean
-  public WebSocketHandlerAdapter handlerAdapter() {
-    return new WebSocketHandlerAdapter();
   }
 }
