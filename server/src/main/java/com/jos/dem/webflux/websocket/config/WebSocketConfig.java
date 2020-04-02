13b82bb8ca7f219ad6ef64b2bf4a8d9d8df1c3eb
josdem <joseluis.delacruz@gmail.com>
2020-04-01 20:42:30 -0400

    #8 Adding websocket configuration

---
+++ server/src/main/java/com/jos/dem/webflux/websocket/config/WebSocketConfig.java
@@ -0,0 +1,36 @@
+package com.jos.dem.webflux.websocket.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.reactive.HandlerMapping;
+import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebSocketConfig {
+
+  private final WebSocketHandler webSocketHandler;
+
+  @Bean
+  public HandlerMapping handlerMapping() {
+    Map<String, WebSocketHandler> map = new HashMap<>();
+    map.put("/channel", webSocketHandler);
+
+    SimpleUrlHandlerMapping mapping = new SimpleUrlHandlerMapping();
+    mapping.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    mapping.setUrlMap(map);
+    return mapping;
+  }
+
+  @Bean
+  public WebSocketHandlerAdapter handlerAdapter() {
+    return new WebSocketHandlerAdapter();
+  }
+}
